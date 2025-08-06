b7a919280deb1b7a06b641a8dc2eb171e30fbdd1
Zishan Faizi <88134937+w-faizi@users.noreply.github.com>
2025-08-06 21:58:09 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Trivia Game!</h1>
+      <p>Edit App.js to add your game.</p>
+    </div>
+  );
+}
+
+export default App;
